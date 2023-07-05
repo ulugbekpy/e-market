146ab0cf7ae1e684b638504fc8b6b6ed5a64de0d
ulugbekpy <ulugbekpycpp@gmail.com>
2023-07-05 10:42:11 +0500

    a few lines of code in admin.py file

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from .models import Category,Product,Shop,Seller
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','description','price']
+    list_per_page = 10
+
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(Shop)
 admin.site.register(Seller)
